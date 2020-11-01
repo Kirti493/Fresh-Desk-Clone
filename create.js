9126e9922f6c52a04a67cf70f3bfeaf3417aca79
Kirti493 <kirtisri493@gmail.com>
2020-11-01 11:45:58 +0530

    Added Delete Item

--- create.js
+++ create.js
@@ -3,9 +3,16 @@ const title = document.getElementById("title");
 const description = document.getElementById("desc");
 const button = document.getElementById("submit");
 
+const details = JSON.parse(localStorage.getItem("details")) || [];
+console.log('Getting Details ',details)
+
+let primaryKey = 1
+
 let name = "";
 let utitle = "";
 let udescription = "";
+
+
 
 username.addEventListener('change', unameupdateValue);
 
@@ -26,24 +33,54 @@ function descupdateValue(e) {
   udescription = e.target.value;
 }
 
-const details = JSON.parse(localStorage.getItem("details")) || [];
+
 
 let data = {
+    userID: "",
     userName: "",
     problemTitle: "",
     problemDescription: ""
 };
 
-
 button.addEventListener('click', value);
 function value(e){
     e.preventDefault();
+
+
+    if(details.length <1){
+      console.log('Entering Main Case')
+      data.userID = 0;
+    }
+
+    else if(details.length >=1){
+      console.log('Entering Case')
+      data.userID = details[details.length-1].userID + 1
+    }
+
+
     data.userName = name;
     data.problemTitle = utitle;
     data.problemDescription = udescription;
+
+    console.log("Local Storage Details are: ",details)
     details.push(data);
+
+    console.log(data);
+
+    // Update the Local Storage
+
     localStorage.setItem("details", JSON.stringify(details));
     console.log("clicked");
+
+    alert('Raised Ticket Successfully :)')
+
+    setTimeout(() => {
+      window.location.reload()
+    }, 500);
+
+
+
+
 
 }
 
--- view.js
+++ view.js
@@ -1,6 +1,28 @@
-const details = JSON.parse(localStorage.getItem("details"));
+let details = JSON.parse(localStorage.getItem("details"));
 console.log(details)
 const view = document.getElementById("view");
+
+
+
+const removeCard = (id) => {
+    console.log('Getting the Card id')
+    console.log(id)
+    let updatedDetails = []
+    details.forEach((detail)=>{
+      if(detail.userID === id){
+        details = details.splice(id-1,1);
+        localStorage.setItem("details", JSON.stringify(details));
+        window.location.reload()
+
+      }else{
+        updatedDetails.push(detail)
+      }
+    })
+    console.log("Updated Details",updatedDetails)
+}
+
+
+
 
 let html = '';
 details.forEach(detail => {
@@ -12,12 +34,13 @@ details.forEach(detail => {
         <img src="user1.PNG" alt="user1" id="user">
       </div>
       <div class="col-md-9">
+        <p id="line">User ID: ${detail.userID}</p>
         <p id="line">User Name: ${detail.userName}</p>
         <p id="line">Title: ${detail.problemTitle}</p>
         <p id="line">Description: ${detail.problemDescription}</p>
-        <span class="material-icons" id="edit">
+        <span class="material-icons" id="edit" >
           edit
-        <span class="material-icons" id="cancel">
+        <span class="material-icons" id="cancel" onclick="removeCard(${detail.userID})" >
           cancel
           </span>
         </span>
